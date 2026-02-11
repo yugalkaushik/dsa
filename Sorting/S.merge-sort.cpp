b1394e6dd421ef9183a6b2ee3897f8374e7aaee5
Yugal Kaushik <yugalkaushik14@gmail.com>
2026-02-11 18:22:52 +0530

    Merge Sort

---
+++ Sorting/S.merge-sort.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void merge(vector<int>& nums, int low, int mid, int high){
+        vector<int> temp;
+        int left = low;
+        int right = mid+1;
+
+        while(left <=mid && right<=high){
+            if(nums[left] <= nums[right]) {
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else {
+            temp.push_back(nums[right]);
+            right++;
+            }
+        }
+        while(left<=mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.push_back(nums[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            nums[i] = temp[i-low];
+        }
+    }
+
+    void helper(vector<int>& nums, int low, int high){
+        if(low >= high) return;
+        int mid = (low+high)/2;
+        helper(nums,low,mid);
+        helper(nums,mid+1,high);
+        merge(nums,low,mid,high);
+    }
+
+    vector<int> mergeSort(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-1;
+        helper(nums,low,high);
+        return nums;
+    }
+};
+
+//tc = O(N log N) due to divide and conquer approach, O(N) for merging the two halves
+//sc = O(N) for the temporary array used for merging, O(log N) for the recursive call stack
