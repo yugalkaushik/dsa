eee93d0b7ee19ef2679b0c6c5f0cd347ed270bb8
Yugal Kaushik <yugalkaushik14@gmail.com>
2026-03-25 15:39:50 +0530

    Maximum Subarray

---
+++ Arrays/53.Maximum-Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result = INT_MIN;
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            result = max(result,sum);
+            if(sum < 0) sum = 0;
+        }
+        return result;
+    }
+};
+// tc: O(n)
+// sc: O(1)
