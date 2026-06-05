a78bdd059e7b6a1534c696a6c4968c596140e6e9
yugalkaushik <yugalkaushik14@gmail.com>
2026-06-05 13:00:46 +0530

    Jump Game VI

---
+++ Queue/1696.Jump-Game-VI.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        deque<int> dq;
+        dq.push_back(0);
+        for(int i=1;i<n;i++){
+            while(!dq.empty() && dq.front() < i-k){
+                dq.pop_front();
+            }
+            dp[i] = nums[i] + dp[dq.front()];
+
+            while(!dq.empty() && dp[dq.back()]<=dp[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        return dp[n-1];
+    }
+};
+// tc = O(n)
+// sc = O(n)
